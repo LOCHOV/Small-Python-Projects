44df282e6d6685b3086f9b168d2c4188dadfffef
LOCHOV <48244644+LOCHOV@users.noreply.github.com>
2021-01-23 22:18:37 +0100

    Add files via upload

---
+++ digest-auth-hash-creation-RFC2069.py
@@ -0,0 +1,33 @@
+# This script explains how the response hash is created on http-digest-authenticatino on RFC2069
+
+# Compute digest authentication hash creation (md5)
+
+import hashlib
+'''
+You can pick the data from wireshark filtering for "http" after 
+you have done a loggin attempt with http-digest-authentication
+hash1 is a md5 hash of "username:realm:password"
+'''
+
+hash1_string = 'username:realm:password'
+hash1 = hashlib.md5(hash1_string.encode('utf-8')).hexdigest()
+print("hash1_ "+hash1)
+
+# hash 2 consists "of method:URI"
+hash2_string = 'method:/uri'
+hash2 = hashlib.md5(hash2_string.encode('utf-8')).hexdigest()
+print("hash2_ " + hash2)
+
+# the result (RESPONSE) if an md5 hash that consists of:
+# hash1:nonce:hash2
+
+nonce = "nonce"
+
+response_string = hash1 + ":" + nonce + ":" + hash2
+response = hashlib.md5(response_string.encode('utf-8')).hexdigest()
+print("response_ "+response)
+
+'''
+it is possible to try to reverse the hash via rainbow table. 
+However it's not recommendable since there is risk of MD5 collision.
+'''
